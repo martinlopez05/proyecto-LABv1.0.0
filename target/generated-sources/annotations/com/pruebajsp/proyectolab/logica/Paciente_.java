c9eb91d76856193e5dd72da04fd385d733832307
martinlopez05 <martinlopez45630@gmail.com>
2025-03-21 14:40:49 -0300

    images

--- target/generated-sources/annotations/com/pruebajsp/proyectolab/logica/Paciente_.java
+++ target/generated-sources/annotations/com/pruebajsp/proyectolab/logica/Paciente_.java
@@ -6,7 +6,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-03-21T10:51:01", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-03-21T14:16:17", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Paciente.class)
 public class Paciente_ { 
 
